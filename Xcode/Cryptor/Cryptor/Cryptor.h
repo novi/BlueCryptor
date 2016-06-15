bbdce0e78237e3e8f27f52f5665cab0c5c50d759
Yusuke Ito <novi.mad@gmail.com>
2016-06-15 14:39:22 +0900

    add .framework based xcodeproj

---
+++ Xcode/Cryptor/Cryptor/Cryptor.h
@@ -0,0 +1,19 @@
+//
+//  Cryptor.h
+//  Cryptor
+//
+//  Created by Yusuke Ito on 6/15/16.
+//  Copyright © 2016 Yusuke Ito. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Cryptor.
+FOUNDATION_EXPORT double CryptorVersionNumber;
+
+//! Project version string for Cryptor.
+FOUNDATION_EXPORT const unsigned char CryptorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cryptor/PublicHeader.h>
+
+
